284e093b65ed8dca04c9cabb32850230cc0bca93
Marko Lozina <marko@email.com>
2025-12-11 18:07:28 +0100

    svega11 commit

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY,
-  {
-    auth: { persistSession: false },
-  }
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  { auth: { persistSession: false } }
 );
